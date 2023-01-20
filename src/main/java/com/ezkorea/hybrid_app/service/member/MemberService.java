7e8b0bcf67ee0c6f66bbf8943c44b421db734073
Jwhyee <kawnsdud@naver.com>
2023-01-21 00:11:34 +0900

    feat : Create member

--- src/main/java/com/ezkorea/hybrid_app/service/member/MemberService.java
+++ src/main/java/com/ezkorea/hybrid_app/service/member/MemberService.java
@@ -1,9 +1,11 @@
 package com.ezkorea.hybrid_app.service.member;
 
 import com.ezkorea.hybrid_app.domain.member.Member;
+import com.ezkorea.hybrid_app.domain.member.MemberRepository;
 import com.ezkorea.hybrid_app.web.dto.SignUpDto;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,13 +13,21 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final ModelMapper mapper;
+    private final PasswordEncoder passwordEncoder;
+
+    private final MemberRepository memberRepository;
+
 
     /**
      * 회원가입을 하기 위한 메소드
-     *
      * @param dto sign-up.html에서 받아온 정보
      */
     public Member saveNewMember(SignUpDto dto) {
-        return mapper.map(dto, Member.class);
+        dto.setPassword(passwordEncode(dto.getPassword()));
+        return memberRepository.save(mapper.map(dto, Member.class));
+    }
+
+    public String passwordEncode(String password) {
+        return passwordEncoder.encode(password);
     }
 }
--- src/main/java/com/ezkorea/hybrid_app/web/LoginController.java
+++ src/main/java/com/ezkorea/hybrid_app/web/LoginController.java
@@ -1,10 +1,23 @@
 package com.ezkorea.hybrid_app.web;
 
+import com.ezkorea.hybrid_app.service.member.MemberService;
+import com.ezkorea.hybrid_app.web.dto.SignUpDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/")
+    public String showMainPage() {
+        return "index";
+    }
+
     @GetMapping("/login")
     public String showLoginPage() {
         return "login/login";
@@ -13,5 +26,11 @@ public class LoginController {
     @GetMapping("/signup")
     public String showSignUpPage() {
         return "login/sign-up";
+    }
+
+    @PostMapping("/signup")
+    public String doSignUp(SignUpDto dto) {
+        memberService.saveNewMember(dto);
+        return "redirect:/login";
     }
 }
