4cdd3ba2477bf76d557f98e5bb3646b729ac9f04
Jwhyee <kawnsdud@naver.com>
2023-01-24 21:07:27 +0900

    feat : attendance

--- src/main/java/com/ezkorea/hybrid_app/domain/member/SecurityUser.java
+++ src/main/java/com/ezkorea/hybrid_app/domain/member/SecurityUser.java
@@ -1,6 +1,7 @@
 package com.ezkorea.hybrid_app.domain.member;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
@@ -8,11 +9,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@Slf4j
 public class SecurityUser extends User {
     private final Long id;
     private final String username;
     private final String name;
     private final String sex;
+    private final boolean isAttendance;
     private final LocalDateTime createDate;
 
     public SecurityUser(Member member, List<GrantedAuthority> authorities) {
@@ -20,6 +23,7 @@ public class SecurityUser extends User {
         this.id = member.getId();
         this.sex = member.getSex();
         this.name = member.getName();
+        this.isAttendance = member.isAttendance();
         this.username = member.getUsername();
         this.createDate = member.getCreateDate();
     }
@@ -31,6 +35,7 @@ public class SecurityUser extends User {
                 .name(name)
                 .sex(sex)
                 .username(username)
+                .isAttendance(isAttendance)
                 .build();
     }
 }
