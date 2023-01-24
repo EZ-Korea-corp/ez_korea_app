87a2210e0c75c6f0263783c33034ec370d052369
Jwhyee <kawnsdud@naver.com>
2023-01-25 03:03:06 +0900

    feat : pick sell price

--- src/main/java/com/ezkorea/hybrid_app/domain/sale/WiperSort.java
+++ src/main/java/com/ezkorea/hybrid_app/domain/sale/WiperSort.java
@@ -9,9 +9,9 @@ public enum WiperSort {
 
     NORMAL("normal", "일반", 20000),
     HYBRID("hybrid", "하이브리드", 30000),
-    SIZE_700("size700", "700 사이즈", 30000),
+    SIZE_700("size_700", "칠백 사이즈", 30000),
     MW("mw", "엠떠블유(MW)", 40000),
-    S_CLASS("sclass", "S클래스", 80000);
+    S_CLASS("s_class", "S클래스", 80000);
 
     private final String name;
     private final String viewName;
