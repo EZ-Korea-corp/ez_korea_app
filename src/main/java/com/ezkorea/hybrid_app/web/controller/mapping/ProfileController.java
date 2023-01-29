8a35a4808d88a4a70713adf749a4de9608218a62
Jwhyee <kawnsdud@naver.com>
2023-01-29 23:08:18 +0900

    feat : Member profile page

--- src/main/java/com/ezkorea/hybrid_app/web/controller/mapping/ProfileController.java
+++ src/main/java/com/ezkorea/hybrid_app/web/controller/mapping/ProfileController.java
@@ -1,9 +1,13 @@
 package com.ezkorea.hybrid_app.web.controller.mapping;
 
+import com.ezkorea.hybrid_app.domain.user.member.Member;
+import com.ezkorea.hybrid_app.service.user.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,8 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class ProfileController {
 
-    @GetMapping("")
-    public String showProfileMainPage() {
+    private final MemberService memberService;
+
+    @GetMapping("/{username}")
+    public String showMemberProfilePage(@PathVariable String username, Model model) {
+        Member currentMember = memberService.findByUsername(username);
+        model.addAttribute("user", currentMember);
         return "profile/main";
     }
+
 }
