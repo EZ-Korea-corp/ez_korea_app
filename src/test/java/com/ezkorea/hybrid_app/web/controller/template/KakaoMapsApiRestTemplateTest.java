8c044423bf31f78f199f92aa7eca312fd4e690f2
Jwhyee <kawnsdud@naver.com>
2023-02-07 17:30:35 +0900

    refactor : test properties

--- src/test/java/com/ezkorea/hybrid_app/web/controller/template/KakaoMapsApiRestTemplateTest.java
+++ src/test/java/com/ezkorea/hybrid_app/web/controller/template/KakaoMapsApiRestTemplateTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 import java.io.IOException;
 import java.util.Map;
@@ -13,6 +14,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Profile("${test}")
 class KakaoMapsApiRestTemplateTest {
 
     @Autowired
