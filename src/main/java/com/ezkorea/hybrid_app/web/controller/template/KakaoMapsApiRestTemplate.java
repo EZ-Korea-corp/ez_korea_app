3ee700ded7ce0e72bca6f6a7fa7eee2e7d2b4bcc
Jwhyee <kawnsdud@naver.com>
2023-02-07 21:49:40 +0900

    doc : add javadoc

--- src/main/java/com/ezkorea/hybrid_app/web/controller/template/KakaoMapsApiRestTemplate.java
+++ src/main/java/com/ezkorea/hybrid_app/web/controller/template/KakaoMapsApiRestTemplate.java
@@ -31,16 +31,34 @@ public class KakaoMapsApiRestTemplate {
         return getAddressName(getJsonObject(getCurrentPosInfo(currentLat, currentLng)));
     }
 
+    /**
+     * 현재 위치에서 회사까지의 정보를 받아오는 메소드
+     * @param currentLat JS를 통해 받아온 위도
+     * @param currentLng JS를 통해 받아온 경도
+     * @return RestTemplate을 통해 받아온 JSON(Map) 결과 반환
+     * */
     public Map<String, Object> getDistanceFromCompany(String currentLat, String currentLng) {
         String url = "https://dapi.kakao.com/v2/local/search/keyword.json?x={currentLng}&y={currentLat}&radius=20000&query=이제트코리아";
         return getRestTemplateResult(url, currentLat, currentLng);
     }
 
+    /**
+     * 현재 위치에 대한 주소 정보를 받아오기 위한 메소드
+     * @param currentLat JS를 통해 받아온 위도
+     * @param currentLng JS를 통해 받아온 경도
+     * @return RestTemplate을 통해 받아온 JSON(Map) 결과 반환
+     * */
     public Map<String, Object> getCurrentPosInfo(String currentLat, String currentLng) {
         String url = "https://dapi.kakao.com/v2/local/geo/coord2address.json?x={currentLng}&y={currentLat}";
         return getRestTemplateResult(url, currentLat, currentLng);
     }
 
+    /**
+     * Kakao Maps Api에 요청을 보내기 위한 함수
+     * @param currentLat 사용자의 위도
+     * @param currentLng 사용자의 경도
+     * @return 조회를 통해 받아온 JSON(Map) 결과 반환
+     * */
     public Map<String, Object> getRestTemplateResult(String url, String currentLat, String currentLng) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -50,6 +68,11 @@ public class KakaoMapsApiRestTemplate {
         return response.getBody();
     }
 
+    /**
+     * Kakao Maps Api 기준으로 받아온 Body를 JSONObject로 변환해주는 메소드
+     * @param resultData RestTemplate을 통해 받아온 JSON(Map)
+     * @return 받아온 데이터를 JSONObject 형태로 반환
+     * */
     public JSONObject getJsonObject(Map<String, Object> resultData) {
         Map<String, Object> objectMap = resultData;
         ObjectMapper mapper = new ObjectMapper();
@@ -65,11 +88,21 @@ public class KakaoMapsApiRestTemplate {
         return datum;
     }
 
+    /**
+     * JSONObject 형식으로 반환된 데이터 중에서 address_name만 받아오는 함수
+     * @param json RestTemplate 요청을 통해 받아와 변환한 데이터
+     * @return 사용자의 address_name을 반환
+     * */
     public String getAddressName(JSONObject json) {
         JSONObject address = (JSONObject) json.get("address");
         return (String) address.get("address_name");
     }
 
+    /**
+     * JSONObject 형식으로 반환된 데이터 중에서 distance만 받아오는 함수
+     * @param json RestTemplate 요청을 통해 받아와 변환한 데이터
+     * @return 사용자의 위치에서 회사까지 distance을 반환
+     * */
     public String getDistance(JSONObject json) {
         return (String) json.get("distance");
     }
