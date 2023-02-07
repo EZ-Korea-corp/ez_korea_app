2e6bedceec47a44b4e3f9b1c73f5e3637ae7a63f
Jwhyee <kawnsdud@naver.com>
2023-02-07 16:16:51 +0900

    test : add pretty print json method

--- src/test/java/com/ezkorea/hybrid_app/web/controller/template/KakaoMapsApiRestTemplateTest.java
+++ src/test/java/com/ezkorea/hybrid_app/web/controller/template/KakaoMapsApiRestTemplateTest.java
@@ -2,13 +2,12 @@ package com.ezkorea.hybrid_app.web.controller.template;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,22 +21,29 @@ class KakaoMapsApiRestTemplateTest {
     private final String lat = "37.2976181";
     private final String lng = "126.9714921";
 
-    @Test
-    void getLocationData() throws JsonProcessingException, JSONException {
-        Map<String, Object> objectMap = template.getDistanceFromCompany(lat, lng);
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonStr = mapper.writeValueAsString(objectMap);
-        JSONObject json = new JSONObject(jsonStr);
-        JSONArray documents = json.getJSONArray("documents");
-        JSONObject object = documents.getJSONObject(0);
-        String distance = object.getString("distance");
-        System.out.println("json object = " + object);
-        System.out.println("distance = " + distance);
+    void printPrettyJson(Map<String, Object> json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String prettyJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+            System.out.println(prettyJson);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
+    @DisplayName("현재 위치 기준 모든 정보를 확인하는 테스트")
+    void getAllInfoJson() {
+        Map<String, Object> objectMap = template.getCurrentPosInfo(lat, lng);
+        printPrettyJson(objectMap);
+    }
+
+    @Test
+    @DisplayName("현재 위치의 도로명을 확인하는 테스트")
     void getAddressName() {
         Map<String, Object> objectMap = template.getCurrentPosInfo(lat, lng);
+        printPrettyJson(objectMap);
+
         org.json.simple.JSONObject jsonObject = template.getJsonObject(objectMap);
         String addressName = template.getAddressName(jsonObject);
         System.out.println("addressName = " + addressName);
@@ -46,10 +52,12 @@ class KakaoMapsApiRestTemplateTest {
     }
 
     @Test
+    @DisplayName("현재 위치에서 회사까지 거리를 확인하는 테스트")
     void getDistance() {
         Map<String, Object> objectMap = template.getDistanceFromCompany(lat, lng);
+        printPrettyJson(objectMap);
+
         org.json.simple.JSONObject jsonObject = template.getJsonObject(objectMap);
-        System.out.println("jsonObject = " + jsonObject);
         String distance = template.getDistance(jsonObject);
         System.out.println("distance = " + distance);
 
