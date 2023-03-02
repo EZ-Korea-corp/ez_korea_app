e790b689f46a5a255120c52d51de0b5fc32bd5ef
Jwhyee <kawnsdud@naver.com>
2023-03-02 15:47:46 +0900

    refactor : file

---
+++ src/test/java/com/ezkorea/hybrid_app/service/member/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package com.ezkorea.hybrid_app.service.member;
+
+import com.ezkorea.hybrid_app.domain.user.member.Member;
+import com.ezkorea.hybrid_app.service.user.member.MemberService;
+import com.ezkorea.hybrid_app.web.dto.SignUpDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MemberServiceTest {
+
+    @Autowired
+    ModelMapper mapper;
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void findMemberByUsernameTest() {
+        long start = System.currentTimeMillis();
+        Member member = memberService.findByUsername("ez_dev_team_master");
+        long end = System.currentTimeMillis();
+        System.out.println("JPA 실행 시간 : " + (end - start)/1000.0);
+        assertThat(member.getName()).isEqualTo("개발팀");
+    }
+
+    @Test
+    void findMemberByIdTest() {
+        long start = System.currentTimeMillis();
+        Member member = memberService.findMemberById(1L);
+        long end = System.currentTimeMillis();
+        System.out.println("JPA 실행 시간 : " + (end - start)/1000.0);
+        assertThat(member.getName()).isEqualTo("개발팀");
+    }
+}
--- src/test/java/com/ezkorea/hybrid_app/web/template/KakaoMapsApiRestTemplateTest.java
+++ src/test/java/com/ezkorea/hybrid_app/web/template/KakaoMapsApiRestTemplateTest.java
@@ -1,4 +1,4 @@
-package com.ezkorea.hybrid_app.web.controller.template;
+package com.ezkorea.hybrid_app.web.template;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -6,16 +6,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Profile("${test}")
+@ActiveProfiles("test")
 class KakaoMapsApiRestTemplateTest {
-    // test
 
     @Autowired
     private KakaoMapsApiRestTemplate template;
@@ -33,14 +32,14 @@ class KakaoMapsApiRestTemplateTest {
         }
     }
 
-//    @Test
+    @Test
     @DisplayName("현재 위치 기준 모든 정보를 확인하는 테스트")
     void getAllInfoJson() {
         Map<String, Object> objectMap = template.getCurrentPosInfo(lat, lng);
         printPrettyJson(objectMap);
     }
 
-//    @Test
+    @Test
     @DisplayName("현재 위치의 도로명을 확인하는 테스트")
     void getAddressName() {
         Map<String, Object> objectMap = template.getCurrentPosInfo(lat, lng);
@@ -53,7 +52,7 @@ class KakaoMapsApiRestTemplateTest {
         assertThat(addressName).isEqualTo("경기 수원시 장안구 율전동 433-2");
     }
 
-//    @Test
+    @Test
     @DisplayName("현재 위치에서 회사까지 거리를 확인하는 테스트")
     void getDistance() {
         Map<String, Object> objectMap = template.getDistanceFromCompany(lat, lng);
