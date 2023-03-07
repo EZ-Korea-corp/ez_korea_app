e15a11ced44446665dd21113a1d521a76fb5f6a8
Jwhyee <kawnsdud@naver.com>
2023-03-08 00:55:03 +0900

    refactor : PathNote view sort by date

--- src/main/java/com/ezkorea/hybrid_app/domain/note/PatchNoteRepository.java
+++ src/main/java/com/ezkorea/hybrid_app/domain/note/PatchNoteRepository.java
@@ -1,7 +1,11 @@
 package com.ezkorea.hybrid_app.domain.note;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatchNoteRepository extends JpaRepository<PatchNote, Long> {
+
+    Page<PatchNote> findAllByOrderByCreateDateDesc(Pageable pageable);
 
 }
